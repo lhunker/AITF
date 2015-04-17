221b342fddc4b78d8aa7fff5616d909a6d077e14
Brett Ammeson <ammesonb@gmail.com>
2015-04-17 14:40:43 -0400

    Fills out basic function structures
      * Switches everything to aitf namespace
      * Adds getters/setters for AITFPacket
      * Adds constructor for AITFPacket

--- shared_libs/aitf_prot.cpp
+++ shared_libs/aitf_prot.cpp
@@ -1,12 +1,81 @@
+#include <openssl/rand.h>
+#include <math.h>
+#include <time.h>
 #include "aitf_prot.h"
 
-Flow::Flow() {
-    std::queue<int> ips(6);
-    std::queue<int> hashes(6);
-}
+using namespace std;
 
-Flow::AddHop(int ip, int hash) {
-    if (ips.size() == ips.capacity()) {ips.pop(); hashes.pop();}
-    ips.push(ip);
-    hashes.push(hash);
+namespace aitf {
+    Flow::Flow() {
+        std::queue<int> ips(6);
+        std::queue<int> hashes(6);
+    }
+
+    Flow::AddHop(int ip, int hash) {
+        if (ips.size() == ips.capacity()) {ips.pop(); hashes.pop();}
+        ips.push(ip);
+        hashes.push(hash);
+    }
+
+    FlowPaths::FlowPaths() {
+        std::vector<int[6]> route_ips(10);
+        std::vector<int> pkt_count(10);
+        std::vector<int> pkt_times(10);
+    }
+
+    FlowPaths::AddFlow(int[6] route) {
+        if (route_ips.size() == route_ips.capacity()) {
+            int cap = route_ips.capacity();
+            route_ips.resize(cap + 10);
+            pkt_count.resize(cap + 10);
+            pkt_times.resize(cap + 10);
+        }
+        route_ips.push_back(route);
+        pkt_count.push_back(0);
+        pkt_times.push_back(time(NULL));
+    }
+
+    FlowPaths::ResetCount() {
+        for (i = 0; i < pkt_count.capacity(); i++) {pkt_count[i] = 0; pkt_time[i] = time(NULL);}
+    }
+
+    AITFPacket::set_mode(unsigned mode::4) {
+        mode = mode;
+    }
+
+    AITFPacket::set_seq(unsigned seq::4) {
+        seq = seq;
+    }
+
+    AITFPacket::set_nonce(unsigned nonce::4) {
+        nonce = nonce;
+    }
+
+    AITFPacket::get_mode() {
+        return mode;
+    }
+
+    AITFPacket::get_seq() {
+        return seq;
+    }
+
+    AITFPacket::get_nonce() {
+        return nonce;
+    }
+
+    AITFPacket::AITFPacket(unsigned mode::4) {
+        mode = mode;
+        srand(time(NULL));
+        set_seq(rand() % (pow(2, 8)));
+        RAND_load_file("/dev/urandom", 1024);
+        RAND_bytes(nonce, 16);
+        Flow flow();
+    }
+
+    AITFPacket::AITFPacket(unsigned mode:4, unsigned seq:16, char nonce[16]) {
+        set_mode(mode);
+        set_seq(seq);
+        set_nonce(nonce);
+        Flow flow();
+    }
 }
--- shared_libs/aitf_prot.h
+++ shared_libs/aitf_prot.h
@@ -1,48 +1,52 @@
 #ifndef __AITF_PROT_H
 #define __AITF_PROT_H
 
-// Network protocol modes
-#define AITF_HELO 0
-#define AITF_CONF 1
-#define AITF_ACK 2
 
 #include <vector.h>
 #include <queue.h>
 
-class Flow {
-    std::queue<int> ips;
-    std::queue<int> hashes;
-    public:
-        Flow();
-        void AddHop(int, int);
+namespace aitf {
+    // Network protocol modes
+    #define AITF_HELO 0
+    #define AITF_CONF 1
+    #define AITF_ACK 2
+
+    class Flow {
+        public:
+            Flow();
+            void AddHop(int, int);
+        private:
+            std::queue<int> ips;
+            std::queue<int> hashes;
+    }
+
+    class FlowPaths {
+        public:
+            FlowPaths();
+            void AddFlow(int[6]);
+        private:
+            std::vector<int[6]> route_ips;
+            std::vector<int> pkt_count;
+            std::vector<int> pkt_times;
+            void ResetCount();
+
+    }
+
+    class AITFPacket {
+        public:
+           AITFPacket(unsigned:4);
+           AITFPacket(unsigned:4,unsigned:16,unsigned:64);
+           unsigned get_mode();
+           unsigned get_seq();
+           unsigned get_nonce();
+        private:
+            unsigned mode:4;
+            unsigned sequence:16;
+            Flow flow;
+            char nonce[16];
+            void set_mode(unsigned:4);
+            void set_seq(unsigned:16);
+            void set_nonce(char[16]);
+    }
 }
-
-class FlowPaths {
-    std::vector<int[6]> route_ips;
-    std::vector<int> pkt_count;
-    std::vector<int> pkt_times;
-    public:
-        FlowPaths();
-        void AddFlow(int[6]);
-    private:
-        void reset_count(int);
-
-}
-
-class AITFPacket {
-    unsigned mode:4;
-    unsigned sequence:16;
-    Flow flow;
-    unsigned nonce:64;
-    public:
-       AITFPacket(unsigned:4);
-       AITFPacket(unsigned:4,unsigned:16,unsigned:64);
-       unsigned get_mode();
-       unsigned get_seq();
-       unsigned get_nonce();
-    private:
-        void set_seq(unsigned:16);
-        void set_nonce(unsigned:64);
-}
-
 #endif
