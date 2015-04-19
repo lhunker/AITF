0edf32614457a5df3507e12a45d5842fdce14450
Brett Ammeson <ammesonb@gmail.com>
2015-04-19 19:26:58 -0400

    Adds required headers for the netfilter functions

--- shared_libs/aitf_nf.h
+++ shared_libs/aitf_nf.h
@@ -1,6 +1,18 @@
 #ifndef __AITF_NF_H
 #define __AITF_NF_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <linux/types.h>
+#include <linux/netfilter.h>
+#include <libnetfilter_queue/libnetfilter_queue.h>
+
 namespace aitf {
     extern struct nfq_handle *nfq_h;
     extern struct nfq_q_handle *nfq_qh;
