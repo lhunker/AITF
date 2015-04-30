337ad81368fe6db71cf1bd1f662bd4cbf138aa3d
Brett Ammeson <ammesonb@gmail.com>
2015-04-30 11:25:52 -0400

    Fixes issues with UDP packet sending

--- router/nfq_router.cpp
+++ router/nfq_router.cpp
@@ -154,21 +154,26 @@ namespace aitf {
         ret = nfq_set_verdict(qh, pkt_id, NF_DROP, 0, NULL);
         if (ret == -1) {printf("Failed to set verdict\n"); return ret;}
 
+        Flow *f = pkt->get_flow();
+        int gateway_ip = 0;
+        for (int i = 0; i < 6; i++) {if (f->ips[i] != 0) {gateway_ip = f->ips[i]; break;}}
+
         int sock = socket(AF_INET, SOCK_DGRAM, 0);
         struct sockaddr_in addr;
         addr.sin_family = AF_INET;
         unsigned char bytes[4];
-        bytes[0] = src_ip & 0xFF;
-        bytes[1] = (src_ip >> 8) & 0xFF;
-        bytes[2] = (src_ip >> 16) & 0xFF;
-        bytes[3] = (src_ip >> 24) & 0xFF;
+        bytes[0] = g_ip & 0xFF;
+        bytes[1] = (g_ip >> 8) & 0xFF;
+        bytes[2] = (g_ip >> 16) & 0xFF;
+        bytes[3] = (g_ip >> 24) & 0xFF;
         char *sock_ip = create_str(20);
         sprintf(sock_ip, "%d.%d.%d.%d", bytes[3], bytes[2], bytes[1], bytes[0]);
         inet_aton(sock_ip, &addr.sin_addr);
         addr.sin_port = htons(AITF_PORT);
         free(sock_ip);
         char *msg = resp.serialize();
-        if (sendto(sock, msg, sizeof(resp), 0, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+        int msg_size = sizeof(int) * 4 + 8 + FLOW_SIZE;
+        if (sendto(sock, msg, msg_size, 0, (struct sockaddr*)&addr, sizeof(addr)) < 0)
             printf("Failed to send AITF response\n");
         return ret;
     }/*}}}*/
